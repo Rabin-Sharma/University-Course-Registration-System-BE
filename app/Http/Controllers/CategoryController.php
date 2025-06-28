d8bc4bd90c25a6643f4360e82abf7b4e7a46a052
Rabin-Sharma <sharmarabin788@gmail.com>
2025-06-28 19:22:25 +0545

    category fetch api

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json([
+            'status' => true,
+            'message' => 'Categories retrieved successfully',
+            'count' => $categories->count(),
+            'categories' => $categories,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/categories', [CategoryController::class, 'index']);
 });
